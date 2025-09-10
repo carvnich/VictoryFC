f11b997a66120fdd69f797ee052e5b3978ef9f7a
Nicholas Carvalho <nicholasjacarvalho@gmail.com>
2025-09-10 09:08:10 -0400

    Add project files.

---
+++ VictoryFC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VictoryFC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
